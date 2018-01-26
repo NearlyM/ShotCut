a49815a572cdb082d3704fd87144d2618f089a98
ningerlei <ningerlei@danale.com>
2018-01-26 15:09:12 +0800

    shotcuts学习

---
+++ ShotCuts/app/src/main/java/com/nel/myapplication/Activity1.java
@@ -0,0 +1,25 @@
+package com.nel.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Description :
+ * CreateTime : 2017/11/15 17:39
+ *
+ * @author ningerlei@danale.com
+ * @version <v1.0>
+ * @Editor : Administrator
+ * @ModifyTime : 2017/11/15 17:39
+ * @ModifyDescription :
+ */
+
+public class Activity1 extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
